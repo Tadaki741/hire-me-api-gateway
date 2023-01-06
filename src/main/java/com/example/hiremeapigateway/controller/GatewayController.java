db16017d8f83bfedf0282b8fc12cba74463acb97
Trung Nguyen <trung.nguyen@codelink.io>
2023-01-06 19:28:20 +0700

    chore: add docker-compose, update cors settings

--- src/main/java/com/example/hiremeapigateway/controller/GatewayController.java
+++ src/main/java/com/example/hiremeapigateway/controller/GatewayController.java
@@ -2,12 +2,16 @@ package com.example.hiremeapigateway.controller;
 
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin("*")
 public class GatewayController {
-
+    @GetMapping
+    public String test() {
+        return "Test from gateway controller";
+    }
     //Handling with user
 
 
